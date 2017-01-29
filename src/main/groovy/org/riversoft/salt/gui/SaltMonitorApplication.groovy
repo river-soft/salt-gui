48c497f0ad654af9ea1e315cc1e47c8f836608c2
Yuriy Shvets <shvets@river-soft.net>
2017-01-29 21:24:55 +0200

    initial commit

---
+++ src/main/groovy/org/riversoft/salt/gui/SaltMonitorApplication.groovy
@@ -0,0 +1,12 @@
+package org.riversoft.salt.gui
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SaltMonitorApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SaltMonitorApplication, args
+	}
+}
