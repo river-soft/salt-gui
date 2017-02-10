1ec70a35bf4726408a6142ef74c522ed4d45494a
korolev_p <korolev@river-soft.net>
2017-02-10 10:38:38 +0200

    SAL-59 add filter minions

--- client/components/minions/MinionsAccepted.js
+++ client/components/minions/MinionsAccepted.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
+import Input from 'muicss/lib/react/input';
 
 export default class MinionsAccepted extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filterList: [],
+            rerender: false
+        }
+    }
+
+    filter(value) {
+        let minions = this.props.acceptedMinions.filter(item => {
+            return item.name.toLowerCase().search(value.toLowerCase()) !== -1;
+        });
+
+        this.setState({
+            filterList: minions,
+            rerender: value.length
+        })
+    }
+
     render() {
 
-        let acceptedMinions = this.props.acceptedMinions;
+        let acceptedMinions = this.state.rerender ? this.state.filterList : this.props.acceptedMinions;
 
         return <div className='right-block-list'>
+            <Input label='Поиск' floatingLabel={true} onChange={e => {::this.filter(e.target.value)}} />
             <div className='block-list block-list__table'>
                 <table width='100%' className='mui-table'>
                     <tbody>
@@ -14,7 +36,6 @@ export default class MinionsAccepted extends Component {
                         <td className='table__head'>NAME</td>
                         <td className='table__head'>GROUP</td>
                     </tr>
-
                     {acceptedMinions ? acceptedMinions.map((item, index) => {
                             return <tr key={index}>
                                 <td>{item.name}</td>
--- client/components/minions/MinionsUnaccepted.js
+++ client/components/minions/MinionsUnaccepted.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
+import Input from 'muicss/lib/react/input';
 
 export default class MinionsUnaccepted extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filterList: [],
+            rerender: false
+        }
+    }
+
+    filter(value) {
+        let minions = this.props.unacceptedMinions.filter(item => {
+            return item.toLowerCase().search(value.toLowerCase()) !== -1;
+        });
+
+        this.setState({
+            filterList: minions,
+            rerender: value.length
+        })
+    }
+
     render() {
 
-        let unacceptedMinions = this.props.unacceptedMinions;
+        let unacceptedMinions = this.state.rerender ? this.state.filterList : this.props.unacceptedMinions;
 
         return <div className='right-block-list'>
+            <Input label='Поиск' floatingLabel={true} onChange={e => {::this.filter(e.target.value)}} />
             <div className='block-list block-list__table'>
                 <table width='100%' className='mui-table'>
                     <tbody>
