d8f49df5be9be7ad6daae1f0c462795056daa8e2
korolev_p <korolev@river-soft.net>
2017-04-13 15:16:23 +0300

    SAL-117 add test four users

--- src/main/groovy/org/riversoft/salt/gui/controller/TestController.groovy
+++ src/main/groovy/org/riversoft/salt/gui/controller/TestController.groovy
@@ -8,17 +8,22 @@ import org.riversoft.salt.gui.calls.modules.Test
 import org.riversoft.salt.gui.client.SaltClient
 import org.riversoft.salt.gui.datatypes.target.Glob
 import org.riversoft.salt.gui.datatypes.target.Target
+import org.riversoft.salt.gui.enums.Permission
 import org.riversoft.salt.gui.model.CreateMinion
 import org.riversoft.salt.gui.repository.MinionGroupRepository
 import org.riversoft.salt.gui.repository.SaltScriptGroupRepository
 import org.riversoft.salt.gui.repository.SaltScriptRepository
+import org.riversoft.salt.gui.repository.UserRepository
 import org.riversoft.salt.gui.results.Result
 import org.riversoft.salt.gui.service.MinionCRUDService
 import org.riversoft.salt.gui.service.MinionGroupService
+import org.riversoft.salt.gui.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.MessageSource
 import org.springframework.context.support.MessageSourceResourceBundle
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
@@ -56,6 +61,9 @@ class TestController {
     @Autowired
     SaltClient saltClient
 
+    @Autowired
+    UserService userService
+
     @RequestMapping('/ping')
     @ResponseBody
     def findScriptByName() {
@@ -77,9 +85,30 @@ class TestController {
 
         def group = minionGroupService.createMinionGroup('default')
 
-        for(int i = 0; i < 50; i++) {
+        for (int i = 0; i < 50; i++) {
 
             minionCRUDService.createMinion(new CreateMinion(name: "minion${i}${i}"), [group])
+        }
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ROOT')")
+    @RequestMapping("/create-user/{user}")
+    createUser(@PathVariable('user') String user) {
+        switch (user) {
+            case 'reviewer':
+                userService.createUser(user, user, 'Reviewer', 'Zytvjueybxtujdsgjkyznm', Permission.reviever())
+                break
+            case 'scriptMaster':
+                userService.createUser(user, user, 'ScriptMaster', 'Crhbgnsvjtdct', Permission.scriptMaster())
+                break
+            case 'minionMaster':
+                userService.createUser(user, user, 'minionMaster', 'Herjdjlbntkmvbymjyjd', Permission.minionMaster())
+                break
+            case 'executor':
+                userService.createUser(user, user, 'executor', 'Pfgeoedctxnjldbuftncz', Permission.executor())
+                break
+            default:
+                return
         }
     }
 }
